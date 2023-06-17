a845aa6e74fa0cfb082647b44c2cd7d70c5f84e0
Aman Lath <amanlath643@gmail.com>
2023-06-17 07:52:33 +0000

    Final project Created in playground.js

--- playground.js
+++ playground.js
@@ -9,24 +9,44 @@ Assessment Requirements
 */
 
 // create a variable to hold your NFT's
-
+    const NftNum = [];
 // this function will take in some values as parameters, create an
 // NFT object using the parameters passed to it for its metadata, 
 // and store it in the variable above.
-function mintNFT () {
-
+function mintNFT (_name, _ShirtSize, _ShirtColor, _bling) {
+    const NFTobj ={
+        "name" : _name,
+        "shirtSize" : _ShirtSize,
+        "ShirtColor" : _ShirtColor,
+        "bling" : _bling
+    };
+    NftNum.push(NFTobj);
+    console.log("Minted : " + _name);
 }
 
 // create a "loop" that will go through an "array" of NFT's
 // and print their metadata with console.log()
 function listNFTs () {
-
+    for(let i = 0; i < NftNum.length; i++){
+        console.log("\nID: \t\t" + (i + 1));
+        console.log("Name: \t\t" + NftNum[i].name);
+        console.log("Shirt Size: \t" + NftNum[i].shirtSize);
+        console.log("Shirt Color: \t" + NftNum[i].ShirtColor);
+        console.log("Bling: \t\t" + NftNum[i].bling);
+    }
 }
 
 // print the total number of NFTs we have minted to the console
 function getTotalSupply() {
-
+    console.log("\nTotal no of NFTs We have: " + NftNum.length);
 }
 
 // call your functions below this line
-
+mintNFT("Adidas", "M", "Black", "Gold Plated");
+mintNFT("Tommy", "L", "Pink", "Silver lined");
+mintNFT("Zara", "XL", "Orange", "Plain");
+mintNFT("Peter England", "M", "Black", "Plain");
+mintNFT("Puma", "L", "Black", "Silver Lined");
+mintNFT("U.S Polo", "M", "white", "Silver Plated");
+listNFTs();
+getTotalSupply();
